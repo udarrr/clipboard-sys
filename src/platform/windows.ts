b928cec0b9a033dbf27f60eab94fa8151c0f08c6
udarrr <sergey.klyshnikov2@gmail.com>
2023-03-04 12:38:24 +0300

    code refactoring

--- src/platform/windows.ts
+++ src/platform/windows.ts
@@ -5,80 +5,67 @@ import { FilesActionEnum, FilesActionType, SysClipboard } from '../..';
 
 export default class WindowsClipboard implements SysClipboard {
   async readText(): Promise<string> {
+    let result;
+
     try {
-      const { stdout, stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--read'], {
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--read'], {
         stripFinalNewline: true,
       });
-
-      if (stderr) {
-        throw new Error(`cannot read text from clipboard error: ${stderr}`);
-      }
-
-      return stdout;
     } catch {
-      const { stdout, stderr } = await execa(
+      console.log('A try with fallback with powershell');
+
+      result = await execa(
         `powershell -Command Add-Type -AssemblyName System.Windows.Forms; "if([Windows.Forms.Clipboard]::ContainsText()) {$clip=[Windows.Forms.Clipboard]::GetText(); if ($clip -ne $null) {return $clip }} "`,
         {
           stripFinalNewline: true,
         },
       );
-
-      if (stderr) {
-        throw new Error(`cannot read text from clipboard error: ${stderr}`);
-      }
-      return stdout;
     }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot read text from clipboard because of error out: ${result.stderr}`);
+    }
+
+    return result && 'stdout' in result ? result.stdout : '';
   }
 
   async writeText(text: string): Promise<void> {
+    let result;
+
     try {
-      const { stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--write', text]);
-
-      if (stderr) {
-        throw new Error(`cannot write text due to clipboard error: ${stderr}`);
-      }
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--write', text]);
     } catch {
-      const { stderr } = await execa('powershell -noprofile -command $input|Set-Clipboard', { input: text });
+      console.log('A try with fallback with powershell');
 
-      if (stderr) {
-        throw new Error(`cannot write text due to clipboard error: ${stderr}`);
-      }
+      result = await execa('powershell -noprofile -command $input|Set-Clipboard', { input: text });
+    }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot write text to clipboard because of error out: ${result.stderr}`);
     }
   }
 
   async readImage(file?: string): Promise<Buffer> {
+    let result;
+
     try {
-      const { stdout, stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--readImage']);
-
-      if (stderr) {
-        throw new Error(`cannot read image from clipboard error: ${stderr}`);
-      }
-      let imageBuffer = Buffer.from(stdout, 'base64');
-
-      if (file) {
-        await fs.writeFile(file, imageBuffer);
-
-        return imageBuffer;
-      } else {
-        return imageBuffer;
-      }
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--readImage']);
     } catch {
-      const { stdout, stderr } = await execa(
+      console.log('A try with fallback with powershell');
+
+      result = await execa(
         `powershell -Command Add-Type -AssemblyName System.Windows.Forms; "$clip=[Windows.Forms.Clipboard]::GetImage();if ($clip -ne $null) { $converter = New-Object -TypeName System.Drawing.ImageConverter;$byte_vec = $converter.ConvertTo($clip, [byte[]]); $EncodedText =[Convert]::ToBase64String($byte_vec); return $EncodedText }"`,
       );
+    }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot read image from clipboard error: ${result.stderr}`);
+    }
+    let imageBuffer = Buffer.from(result.stdout, 'base64');
 
-      if (stderr) {
-        throw new Error(`cannot read image from clipboard error: ${stderr}`);
-      }
-      let imageBuffer = Buffer.from(stdout, 'base64');
+    if (file) {
+      await fs.writeFile(file, imageBuffer);
 
-      if (file) {
-        await fs.writeFile(file, imageBuffer);
-
-        return imageBuffer;
-      } else {
-        return imageBuffer;
-      }
+      return imageBuffer;
+    } else {
+      return imageBuffer;
     }
   }
 
@@ -100,7 +87,10 @@ export default class WindowsClipboard implements SysClipboard {
       const { stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--writeImage', path]);
 
       if (stderr) {
+        console.log('A try with fallback with powershell');
+
         const { stderr } = await execa(`powershell -Command Add-Type -AssemblyName System.Windows.Forms; "[Windows.Forms.Clipboard]::SetImage([System.Drawing.Image]::FromFile('${path}'));"`);
+
         if (stderr) {
           throw new Error(`cannot write image to clipboard error: ${stderr}`);
         }
@@ -119,59 +109,47 @@ export default class WindowsClipboard implements SysClipboard {
   }
 
   async readFiles() {
+    let result;
+
     try {
-      const { stdout, stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--readFiles'], { stripFinalNewline: true });
-
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
-
-      if (stdout) {
-        const filePaths = stdout.split(/\r\n/g);
-        filePaths.length && filePaths[filePaths.length - 1] === '' ? filePaths.pop() : filePaths;
-
-        return filePaths;
-      } else {
-        return [];
-      }
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--readFiles'], { stripFinalNewline: true });
     } catch {
-      const { stdout, stderr } = await execa(
+      console.log('A try with fallback with powershell');
+
+      result = await execa(
         `powershell -Command Add-Type -AssemblyName System.Windows.Forms; "if ([Windows.Forms.Clipboard]::ContainsFileDropList()) {$files = [Windows.Forms.Clipboard]::GetFileDropList(); return $files"}`,
         {
           stripFinalNewline: true,
         },
       );
+    }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot read files from clipboard error: ${result.stderr}`);
+    }
 
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
+    if (result.stdout) {
+      const filePaths = result.stdout.split(/\r\n/g);
+      filePaths.length && filePaths[filePaths.length - 1] === '' ? filePaths.pop() : filePaths;
 
-      if (stdout) {
-        const filePaths = stdout.split(/\r\n/g);
-        filePaths.length && filePaths[filePaths.length - 1] === '' ? filePaths.pop() : filePaths;
-
-        return filePaths;
-      } else {
-        return [];
-      }
+      return filePaths;
+    } else {
+      return [];
     }
   }
 
   async pasteFiles(action: FilesActionType, destinationFolder: string, ...files: Array<string>): Promise<void> {
+    let result;
+
     if (files && files.length) {
       await this.writeFiles(...files);
     }
 
     try {
-      const { stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--moveFiles', action, destinationFolder], {
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--moveFiles', action, destinationFolder], {
         stripFinalNewline: false,
       });
-
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
     } catch {
-      const { stderr } = await execa(
+      result = await execa(
         `powershell -Command Add-Type -AssemblyName System.Windows.Forms; "$fileDrop = get-clipboard -Format FileDropList; if($fileDrop -eq $null) { write-host 'No files on the clipboard'; return } foreach($file in $fileDrop) {if (Test-Path $file) {if($file.Mode.StartsWith('d')) { $source = join-path $file.Directory $file.Name; Invoke-Expression '${
           action === 'Copy' ? 'copy' : 'move'
         }-item -Recurse $source $($file.Name)'} else {$file.Name; $file | ${action === FilesActionEnum.Copy ? 'copy' : 'move'}-item -Destination "${destinationFolder}"}}}"`,
@@ -179,28 +157,23 @@ export default class WindowsClipboard implements SysClipboard {
           stripFinalNewline: false,
         },
       );
-
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
+    }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot read files from clipboard error: ${result.stderr}`);
     }
   }
 
   async writeFiles(...files: Array<string>): Promise<boolean> {
+    let result;
     const formattedFiles: Array<string> = [];
 
     try {
-      const { stdout, stderr } = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--writeFiles', files.join(',')], { stripFinalNewline: false });
-
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
-      return +stdout === 0;
+      result = await execa(`${pathLib.join(__dirname, '..', '..', 'bin', 'win_clipboard.exe')}`, ['--writeFiles', files.join(',')], { stripFinalNewline: false });
     } catch {
       files.forEach((f) => {
         formattedFiles.push(`('${f}')`);
       });
-      const { stdout, stderr } = await execa(
+      result = await execa(
         `powershell -Command Add-Type -AssemblyName System.Windows.Forms; "$files = [System.Collections.Specialized.StringCollection]::new(); $files.AddRange(@(${formattedFiles.join(
           ',',
         )})); [Windows.Forms.Clipboard]::SetFileDropList($files);`,
@@ -208,11 +181,10 @@ export default class WindowsClipboard implements SysClipboard {
           stripFinalNewline: false,
         },
       );
-
-      if (stderr) {
-        throw new Error(`cannot read files from clipboard error: ${stderr}`);
-      }
-      return +stdout === 0;
     }
+    if (result && 'stderr' in result && result.stderr) {
+      throw new Error(`cannot read files from clipboard error: ${result.stderr}`);
+    }
+    return result && 'stdout' in result && +result.stdout === 0;
   }
 }
